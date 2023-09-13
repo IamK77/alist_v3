73c9e9b41cbb9384004c1331a7b4a4a761dc3bea
lamk77 <chenyinfeng50@gmail.com>
2023-09-14 00:12:37 +0800

    Upload PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     description="Write for alist V3",
     author="Iamk77",
-    author_email="245958742@qq.com",
+    author_email="chenyinfeng50@qq.com",
     url="https://github.com/IamK77/alist_v3",
     packages=find_packages(),
     install_requires=[
